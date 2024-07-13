303f52f500d8234dcd028d4e3628c89d351f99aa
BenicioCA <benicio15cr@gmail.com>
2024-07-13 00:32:58 -0600

    Push 8 (Creación de home, server y css nuevos)

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 const express = require("express")
 const app = express()
+const { v4: uuidv4} = require('uuid');
 
 app.set("view engine", "ejs")
 
+app.use(express.static('public'));
+app.use(express.json());
+
+let notes = [
+    {
+        id: uuidv4(),
+        title: "Nota 1",
+        content: "Contenido de la nota 1",
+        creationDate: new Date().toLocaleString(),
+        lastModifiedDate: new Date().toLocaleDateString(),
+        tags: ["ejemplo", "nota"]
+    }, 
+    {
+        id: uuidv4(),
+        title: "Nota 2",
+        content: "Contenido de la nota 2",
+        creationDate: new Date().toLocaleTimeString(),
+        lastModifiedDate: new Date().toLocaleString(),
+        tags: ["test"]
+    }
+];
+
 app.get("/", (req, res) => {
-    console.log("Running on port: 3000")
-    res.render("index")
-})
+    res.render("home", {notes});
+});
 
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
